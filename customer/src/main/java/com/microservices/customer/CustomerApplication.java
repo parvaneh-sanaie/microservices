7a67db082c447e8cbd03e06eab55f3ec3d92c6aa
Parvaneh Sanaie <parvaneh.sanaie@testifi.io>
2023-07-14 16:46:13 +0200

    add customer model

--- customer/src/main/java/com/microservices/customer/CustomerApplication.java
+++ customer/src/main/java/com/microservices/customer/CustomerApplication.java
@@ -1,4 +1,4 @@
-package com.amigoscode.customer;
+package com.microservices.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
